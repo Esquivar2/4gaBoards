0a776d3e493c822bbdcc8629bfe566f4c6428846
Esquivar <s26294@pjwstk.edu.pl>
2025-10-26 20:07:45 +0100

    add MailStep with reset/update button, fix backend mail update

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -34,10 +34,12 @@ const List = React.memo(
     updatedAt,
     updatedBy,
     boardMemberships,
+    mailId,
     onUpdate,
     onDelete,
     onCardCreate,
     onMailCreate,
+    onMailUpdate,
   }) => {
     const [t] = useTranslation();
     const [isAddCardOpen, setIsAddCardOpen] = useState(false);
@@ -223,9 +225,11 @@ const List = React.memo(
                       updatedAt={updatedAt}
                       updatedBy={updatedBy}
                       boardMemberships={boardMemberships}
+                      mailId={mailId}
                       onNameEdit={handleNameEdit}
                       onCardAdd={handleCardAdd}
                       onMailCreate={onMailCreate}
+                      onMailUpdate={onMailUpdate}
                       onDelete={onDelete}
                       position="left-start"
                       offset={0}
@@ -269,10 +273,12 @@ List.propTypes = {
   updatedAt: PropTypes.instanceOf(Date),
   updatedBy: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   boardMemberships: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+  mailId: PropTypes.string,
   onUpdate: PropTypes.func.isRequired,
   onDelete: PropTypes.func.isRequired,
   onCardCreate: PropTypes.func.isRequired,
   onMailCreate: PropTypes.func.isRequired,
+  onMailUpdate: PropTypes.func.isRequired,
 };
 
 List.defaultProps = {
@@ -280,6 +286,7 @@ List.defaultProps = {
   createdBy: undefined,
   updatedAt: undefined,
   updatedBy: undefined,
+  mailId: null,
 };
 
 export default List;
